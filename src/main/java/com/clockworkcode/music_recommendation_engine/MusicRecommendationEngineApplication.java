f70f478b1a72feb395d14e4f9c27a221a152091d
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2023-01-23 18:39:22 +0100

    Initial commit

---
+++ src/main/java/com/clockworkcode/music_recommendation_engine/MusicRecommendationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.music_recommendation_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecommendationEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicRecommendationEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clockworkcode/music_recommendation_engine/MusicRecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.music_recommendation_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRecommendationEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
